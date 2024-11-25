78be4b918e68118f9bd43710c2dc33e4d861a7cf
anishossain515 <amirul447700@gmail.com>
2024-11-25 11:21:27 +0600

    BG Image Problem Solve

--- src/Components/Offer/Offer.jsx
+++ src/Components/Offer/Offer.jsx
@@ -3,7 +3,7 @@ import OfferCard from './OfferCard'
 import { OfferCardData } from '../Utilities/Data'
 function Offer() {
   return (
-    <section className='mt-[128px] max-w-[1400px] mx-auto'>
+    <section className='sm:mt-[128px] mt-20 max-w-[1400px] mx-auto'>
         <div className='flex flex-col items-center justify-center '>
             <h2 className='font-semibold sm:text-[40px] text-3xl text-center'>What do we offer?</h2>
            <div className='flex flex-wrap gap-8 justify-center pt-[64px] px-2'>
